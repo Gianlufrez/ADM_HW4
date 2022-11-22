e09dfaac50a4eda8ff100948dc2fdd7389c8c422
jonasbarth <jonas.barth.95@gmail.com>
2022-11-22 22:48:14 +0100

    Add logic for finding men's and women's expenditure

--- CommandLine.sh
+++ CommandLine.sh
@@ -9,3 +9,15 @@
 most_common_place=$(awk -F ',' '{print $5}' data/bank_transactions.csv | sort | uniq -c | sort -bnr | head -1)
 
 echo $most_common_place
+
+# Did females spend more than males, or vice versa?
+## Get the gender and the transaction amount columns
+## Sum up the transaction amounts for men and women
+## Compare the amounts
+men_total_expenditure=$(awk -F "," '{print $4, $9}' data/bank_transactions.csv | awk '{if ($1=="M") {print $2}}' | awk '{s+=$1} END {print s}')
+
+women_total_expenditure=$(awk -F "," '{print $4, $9}' data/bank_transactions.csv | awk '{if ($1=="F") {print $2}}' | awk '{s+=$1} END {print s}')
+
+echo "Men spent: $men_total_expenditure"
+echo "Women spent: $women_total_expenditure"
+
