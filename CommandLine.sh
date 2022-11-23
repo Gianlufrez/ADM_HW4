d94bee3c712efcb0c2a6e4b6e1d753f709fb9a9f
jonasbarth <jonas.barth.95@gmail.com>
2022-11-23 19:18:39 +0100

    Command Line * make output pretty * add logic for printing whether men or women spent more

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,16 +1,22 @@
 #!bin/bash
-
-# Which place has the most transactions associated with it?
+# 1. Which place has the most transactions associated with it?
 ## Get the location column
 ## Sort them to prepare for the uniq command
 ## count unique occurrences
 ## sort in numeric reverse order
 ## pick the first element which will be the places with the most transcations 
 most_common_place=$(awk -F ',' '{print $5}' data/bank_transactions.csv | sort | uniq -c | sort -bnr | head -1)
+place=$(awk -F " " '{print $2}' <<< "$most_common_place")
+amount=$(awk -F " " '{print $1}' <<< "$most_common_place")
 
-echo $most_common_place
+printf -- '-%.0s' {1..50}
+printf '\n'
+echo "1. Which place has the most transactions associated with it?"
+echo "The most common place is '$place' with '$amount' transactions."
+printf -- '-%.0s' {1..50}
+printf '\n'
 
-# Did females spend more than males, or vice versa?
+#2. Did females spend more than males, or vice versa?
 ## Get the gender and the transaction amount columns
 ## Sum up the transaction amounts for men and women
 ## Compare the amounts
@@ -18,6 +24,20 @@ men_total_expenditure=$(awk -F "," '{print $4, $9}' data/bank_transactions.csv |
 
 women_total_expenditure=$(awk -F "," '{print $4, $9}' data/bank_transactions.csv | awk '{if ($1=="F") {print $2}}' | awk '{s+=$1} END {print s}')
 
-echo "Men spent: $men_total_expenditure"
-echo "Women spent: $women_total_expenditure"
+# When summing the total, we get scientific notation. Creating a sequence gives us the actual number
+men=$(seq "$men_total_expenditure" "$men_total_expenditure")
+women=$(seq "$women_total_expenditure" "$women_total_expenditure")
 
+echo "2. Who spent more, males or females?"
+echo "Men spent: $men INR"
+echo "Women spent: $women INR"
+
+if [[ $men -gt $women ]]
+then
+	echo "Men spent more than women."
+else
+	echo "Women spent more than men."
+fi
+
+printf -- '-%.0s' {1..50}
+printf '\n'
